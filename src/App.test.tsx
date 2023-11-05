49727ed4ef093af66e85c2f255d92cfef8606bd1
Mihail Davidevski <majkl_david@hotmail.com>
2023-11-05 14:33:22 +0100

    feat: add counter, styles and tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders App with initial count value", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const countDisplay = screen.getByTestId("count");
+  expect(countDisplay).toHaveTextContent("Count: 0");
+});
+
+test("increments and decrements count correctly", () => {
+  render(<App />);
+  const countDisplay = screen.getByTestId("count");
+  const incrementButton = screen.getByTestId("increment");
+  const decrementButton = screen.getByTestId("decrement");
+
+  fireEvent.click(incrementButton);
+  expect(countDisplay).toHaveTextContent("Count: 1");
+
+  fireEvent.click(decrementButton);
+  expect(countDisplay).toHaveTextContent("Count: 0");
+});
+
+test("should reset the counter", () => {
+  render(<App />);
+  const incrementButton = screen.getByTestId("increment");
+  const countDisplay = screen.getByTestId("count");
+  const resetButton = screen.getByTestId("reset");
+  //first check if counter is 0
+  expect(countDisplay).toHaveTextContent("Count: 0");
+  //then update the counter + 2 and check if its 2
+  fireEvent.click(incrementButton);
+  fireEvent.click(incrementButton);
+  expect(countDisplay).toHaveTextContent("Count: 2");
+  //then reset the counter and check if it's 0
+  fireEvent.click(resetButton);
+  expect(countDisplay).toHaveTextContent("Count: 0");
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,54 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello from the app!</div>;
+  const [counter, setCounter] = useState(0);
+
+  const counterHandler = (type: "increment" | "decrement" | "reset") => {
+    if (type === "reset") {
+      setCounter(0);
+      return;
+    }
+    setCounter((prevCounter) =>
+      type === "increment"
+        ? prevCounter + 1
+        : prevCounter === 0
+        ? 0
+        : prevCounter - 1
+    );
+  };
+
+  return (
+    <>
+      <div className="counterContainer">
+        <h1>Counter App</h1>
+        <button
+          className="button"
+          onClick={() => counterHandler("increment")}
+          data-testid="increment"
+        >
+          +
+        </button>
+        <h2 className="count" data-testid="count">
+          Count: {counter}
+        </h2>
+        <button
+          className="button"
+          onClick={() => counterHandler("decrement")}
+          data-testid="decrement"
+        >
+          -
+        </button>
+        <button
+          className="button resetButton"
+          onClick={() => counterHandler("reset")}
+          data-testid="reset"
+        >
+          Reset counter
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
